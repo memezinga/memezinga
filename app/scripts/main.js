300e71054246a9e5dde2f28d676b02784e3ac6ca
Teba Gomez <gomez.teba@gmail.com>
2019-02-19 13:25:41 +0000

    Basic Routing added. #26

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,3 +11,33 @@ window.addEventListener('scroll', function(event) {
         nav.classList.remove('scrolled');
     }
 });
+
+// Router
+var container = document.getElementById("main-wrapper");
+
+var root = null;
+var useHash = true; // Defaults to: false
+var hash = '#!'; // Defaults to: '#'
+var router = new Navigo(root, useHash, hash);
+
+router
+  .on({
+    'generator/:id': function () {
+      setContent('Generator');
+    },
+    'gallery': function () {
+      setContent('Gallery');
+    },
+    '/': function () {
+      setContent('Home');
+    },
+    'download/:id': function () {
+      setContent('Download');
+    }
+  })
+  //.notFound();
+  .resolve();
+  
+function setContent(name){
+    container.innerHTML = name;
+};
