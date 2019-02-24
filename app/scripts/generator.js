7090a3d271a0630d6776351c357d08857f7c2985
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2019-02-24 21:24:39 +0100

    Updating URL with query params. Related to #9 and #10

--- app/scripts/generator.js
+++ app/scripts/generator.js
@@ -1,27 +1,54 @@
 (function() {
+  let routerInstance = {};
+  let selectedMeme = {};
+
+  /**
+   * Default properties values to be set in as query elements in the url
+   */
   const properties = {
     fontSizeTopText: 50,
     fontSizeBottomText: 50,
     selectedColor: '#ffffff',
-    selectedFontFamily: 'BADABB'
+    selectedFontFamily: 'BADABB',
+    topText: '',
+    bottomText: ''
   };
 
+  /**
+   * Gets the top input DOM element and passes it as parameter to the createText function
+   * @returns {void}
+   * @see #createText function
+   */
   function createTopText() {
     const topInput = document.querySelector('.topText');
     createText(topInput, 'top');
   }
 
+  /**
+   * Gets the bottom input DOM element and passes it as parameter to the createBottomText
+   * @returns {void}
+   * @see #createText function
+   */
   function createBottomText() {
     const bottomInput = document.querySelector('.bottomText');
     createText(bottomInput, 'bottom');
   }
 
+  /**
+   * Sets the text from the top input or bottom input target value
+   * @param {Object} input DOM element
+   * @param {String} spanPosition to identify the top input
+   * @returns {void}
+   * @see #calcTopWidth
+   * @see #calcBottomWidth
+   */
   function createText(input, spanPosition) {
     if (spanPosition === 'top') {
       const textPreviewElem = document.querySelector('.topPreviewText');
       input.addEventListener('keyup', function(event) {
         let text = event.target.value;
         textPreviewElem.innerText = text;
+        properties.topText = text;
         calcTopWidth(textPreviewElem, event);
       });
     } else {
@@ -29,11 +56,17 @@
       input.addEventListener('keyup', function(event) {
         let text = event.target.value;
         textPreviewElem.innerText = text;
+        properties.bottomText = text;
         calcBottomWidth(textPreviewElem, event);
       });
     }
   }
 
+  /**
+   * Dynamically calculates the font size for the meme's top preview text
+   * @param {Object} textPreviewElem span DOM element preview element
+   * @param {Object} event mouse key-up event
+   */
   function calcTopWidth(textPreviewElem, event) {
     let imageWidth = document.querySelector('.meme-preview').clientWidth;
     let previewTextWidth = textPreviewElem.offsetWidth;
@@ -57,6 +90,11 @@
     }
   }
 
+  /**
+   * Dynamically calculates the font size for the meme's bottom preview text
+   * @param {Object} textPreviewElem span DOM element preview element
+   * @param {Object} event mouse key-up event
+   */
   function calcBottomWidth(textPreviewElem, event) {
     let imageWidth = document.querySelector('.meme-preview').clientWidth;
     let previewTextWidth = textPreviewElem.offsetWidth;
@@ -73,7 +111,6 @@
         imageWidth > previewTextWidth + 16 &&
         properties.fontSizeBottomText < 50
       ) {
-        console.log('inside bottom DELETE while');
         properties.fontSizeBottomText = properties.fontSizeBottomText + 1;
         textPreviewElem.style.fontSize = `${properties.fontSizeBottomText}px`;
         previewTextWidth = textPreviewElem.offsetWidth;
@@ -81,6 +118,10 @@
     }
   }
 
+  /**
+   * Selects a color from the DOM color picker element
+   * @returns {void}
+   */
   function selectTextColor() {
     const colorPicker = document.querySelector('input[type=color]');
     const topText = document.querySelector('.topPreviewText');
@@ -93,6 +134,11 @@
     });
   }
 
+  /**
+   * Selects either the impact or badabb font family for the preview text
+   * @returns {void}
+   * @see #toggleActiveFontClass
+   */
   function selectFontFamily() {
     const modal = document.querySelector('.dropdown-menu');
     const topText = document.querySelector('.topPreviewText');
@@ -100,7 +146,7 @@
 
     modal.addEventListener('click', function(event) {
       properties.selectedFontFamily = event.target.dataset.id;
-      let isActive = event.target.classList.contains('active');
+
       if (properties.selectedFontFamily === 'impact') {
         topText.style.fontFamily = 'Impact';
         bottomText.style.fontFamily = 'Impact';
@@ -109,15 +155,81 @@
         bottomText.style.fontFamily = 'BADABB';
       }
 
-      isActive && event.target.classList.remove('active');
+      toggleActiveFontClass(event);
     });
   }
 
-  function init() {
+  /**
+   * Toggles the active class in the font's drop down menu
+   * @param {Object} event mouse click event
+   * @returns {void}
+   * @see #selectFontFamily
+   */
+  function toggleActiveFontClass(event) {
+    let target = event.target;
+    let activeTarget = target.classList.contains('active');
+    const targetSibling =
+      event.target.nextElementSibling !== null
+        ? event.target.nextElementSibling
+        : event.target.previousElementSibling;
+
+    if (!activeTarget) {
+      targetSibling.classList.remove('active');
+      target.classList.add('active');
+    }
+  }
+
+  /**
+   * Creates the url query parameters to pass to the next app view and
+   * navigates to the next view
+   * @returns {void}
+   */
+  function createMeme() {
+    const createBtn = document.querySelector('.createButton');
+    const memeId = createBtn.dataset.id;
+
+    createBtn.addEventListener('click', function(event) {
+      const {
+        topText,
+        bottomText,
+        fontSizeTopText,
+        fontSizeBottomText,
+        selectedColor,
+        selectedFontFamily
+      } = properties;
+      const url = `download/${memeId}?topText=${topText}&fontSizeTopText=${fontSizeTopText}&bottomText=${bottomText}&fontSizeBottomText=${fontSizeBottomText}&selectedColor=${selectedColor}&selectedFontFamily=${selectedFontFamily}&imgSrc=${
+        selectedMeme.img_src
+      }`;
+      router.navigate(url);
+    });
+  }
+
+  /**
+   * Sets the default meme's text from the data base in the properties object
+   * @param {Object} meme the selected meme's data from the data base
+   * @returns {void}
+   */
+  function setDefaultText(meme) {
+    properties.topText = decodeURIComponent(meme.topText);
+    properties.bottomText = decodeURIComponent(meme.bottomText);
+  }
+
+  /**
+   * Initialize all functions in the module
+   * @param {Object} router router instance
+   * @param {Object} meme selected meme from the data base
+   * @returns {void}
+   */
+  function init(router, meme) {
+    routerInstance = router;
+    selectedMeme = meme;
+
+    setDefaultText(meme);
     createTopText();
     createBottomText();
     selectTextColor();
     selectFontFamily();
+    createMeme();
   }
 
   let generator = (window.generator = {
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,58 +1,55 @@
-console.log("Hello Memezinga!");
-
+console.log('Hello Memezinga!');
 
 // Navbar effect
 var nav = document.querySelector('.navbar');
 
 window.addEventListener('scroll', function(event) {
-    if (window.scrollY > 50) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
 });
 
 // Login
 
-var loginSltr = document.getElementById("login")
-var logoutSltr = document.getElementById("logout")
+var loginSltr = document.getElementById('login');
+var logoutSltr = document.getElementById('logout');
 
 var auth = firebase.auth();
 var provider = new firebase.auth.GithubAuthProvider();
 var memesRef = firebase.database().ref(`memes`);
 
-loginSltr.addEventListener("click", login)
-logoutSltr.addEventListener("click", logout)
+loginSltr.addEventListener('click', login);
+logoutSltr.addEventListener('click', logout);
 
 auth.onAuthStateChanged(toggleLogin);
 
-function toggleLogin(user){
-    if (user) {
-      // User signed in!
-      logoutSltr.querySelector("img").src = user.photoURL;
-      logoutSltr.style.display = "block";
-      loginSltr.style.display = "none";
-    } else {
-      // User logged out
-      logoutSltr.style.display = "none";
-      loginSltr.style.display = "block";
-    }
+function toggleLogin(user) {
+  if (user) {
+    // User signed in!
+    logoutSltr.querySelector('img').src = user.photoURL;
+    logoutSltr.style.display = 'block';
+    loginSltr.style.display = 'none';
+  } else {
+    // User logged out
+    logoutSltr.style.display = 'none';
+    loginSltr.style.display = 'block';
+  }
 }
 
-function logout () {
+function logout() {
   // return promise!
-  return auth.signOut()
+  return auth.signOut();
 }
 
-function login (){
+function login() {
   // return promise!
-  return auth.signInWithPopup(provider)
+  return auth.signInWithPopup(provider);
 }
-
-
 
 // Router
-var container = document.getElementById("main-wrapper");
+var container = document.getElementById('main-wrapper');
 
 var root = null;
 var useHash = true; // Defaults to: false
@@ -61,36 +58,52 @@ var router = new Navigo(root, useHash, hash);
 
 router
   .on({
-    '/': function () {
+    '/': function() {
       setContent(homeTpl());
       router.updatePageLinks();
     },
-    'gallery': function () {
-        memesRef.orderByKey().limitToFirst(20).once('value', (snapshot) => {
+    gallery: function() {
+      memesRef
+        .orderByKey()
+        .limitToFirst(20)
+        .once('value', snapshot => {
           const memes = Object.values(snapshot.val());
           setContent(galleryTpl(memes));
           router.updatePageLinks();
         });
     },
-    'generator/:id': function (params) {
+    'generator/:id': function(params, query) {
       const memeId = params.id;
-      memesRef.child(memeId).once('value', snapshot =>{
+      memesRef.child(memeId).once('value', snapshot => {
         const meme = snapshot.val();
-          setContent(generatorTpl(meme));
-          router.updatePageLinks();
-          window.generator.init();
+        setContent(generatorTpl(meme));
+        router.updatePageLinks();
+        window.generator.init(router, meme);
       });
     },
-    'download/:id': function () {
-      setContent(downloadTpl());
+    'download/:id': function(params, query) {
+      const memeId = params.id;
+      const selectedMeme = transformQueryToPairs(query);
+      selectedMeme.id = memeId;
+      setContent(downloadTpl(selectedMeme));
       router.updatePageLinks();
     }
   })
   .notFound(function() {
-    router.navigate("/");
+    router.navigate('/');
   })
   .resolve();
 
-function setContent(template){
-    container.innerHTML = template;
-};
+function setContent(template) {
+  container.innerHTML = template;
+}
+
+function transformQueryToPairs(query) {
+  const splitQ = query.split('&');
+  return splitQ.reduce(function(queryObj, queryString) {
+    const queryPairs = queryString.split('=');
+    queryObj[queryPairs[0]] = decodeURIComponent(queryPairs[1]);
+
+    return queryObj;
+  }, {});
+}
